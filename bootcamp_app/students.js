f807a20014108026b25904a0306373376947e080
shtyagi22 <sh.tyagi22@gmail.com>
2022-09-22 21:57:31 -0400

    updated student.js and teachers.js with Parameterized Query

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,12 +7,19 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT id, name, cohort_id
 FROM students
 LIMIT 5;
-`)
-.then(res => {
-  console.log(res);
-})
-.catch(err => console.error('query error', err.stack));
+`;
+
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+pool.query(queryString, values)
+  .then(res => {
+    console.log(res);
+  })
+  .catch(err => console.error('query error', err.stack));
